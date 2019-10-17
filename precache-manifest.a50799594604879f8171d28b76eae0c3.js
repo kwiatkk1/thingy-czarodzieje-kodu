99265f9aa0bab8c9ba8f14bc6cc4bcd1e463ee48
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@allegro.pl>
2019-10-17 14:22:08 +0200

    Updates

---
+++ precache-manifest.a50799594604879f8171d28b76eae0c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdb58c0e31b8dc79a6e61991ed28977",
+    "url": "/thingy-czarodzieje-kodu/index.html"
+  },
+  {
+    "revision": "e141cf0a5faedea6d1d0",
+    "url": "/thingy-czarodzieje-kodu/static/css/main.6580a36d.chunk.css"
+  },
+  {
+    "revision": "2afc52ff773bdbf0b6ef",
+    "url": "/thingy-czarodzieje-kodu/static/js/2.2a83a3e1.chunk.js"
+  },
+  {
+    "revision": "e141cf0a5faedea6d1d0",
+    "url": "/thingy-czarodzieje-kodu/static/js/main.3c6ad7bd.chunk.js"
+  },
+  {
+    "revision": "2a6980aa98cbc777662b",
+    "url": "/thingy-czarodzieje-kodu/static/js/runtime-main.33386c7f.js"
+  }
+]);
